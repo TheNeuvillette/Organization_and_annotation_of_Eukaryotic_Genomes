01ef8b94dfa95f87feefebaba58f4571b16eb74a
TheNeuvilette <imhof.janosch@gmail.com>
2025-10-15 22:48:37 +0200

    Update script

--- scripts/01_Run_EDTA.sh
+++ scripts/01_Run_EDTA.sh
@@ -2,9 +2,9 @@
 
 #SBATCH --job-name=Run_EDTA
 #SBATCH --partition=pibu_el8
-#SBATCH --cpus-per-task=40
-#SBATCH --mem=64G
-#SBATCH --time=12:00:00
+#SBATCH --cpus-per-task=64
+#SBATCH --mem=128G
+#SBATCH --time=2-00:00:00
 #SBATCH --output=logs/%x_%j.out
 #SBATCH --error=logs/%x_%j.err 
 
@@ -12,7 +12,7 @@
 WORKDIR=/data/users/jimhof/Organization_and_annotation_of_Eukaryotic_Genomes
 OUTDIR=$WORKDIR/results/01_EDTA_annotation
 CONTAINER=/data/courses/assembly-annotation-course/CDS_annotation/containers/EDTA2.2.sif
-INPUT_FASTA=$WORKDIR/data/05_02_Hifiasm.bp.p_ctg.fa
+INPUT_FASTA=$WORKDIR/data/Hifiasm.bp.p_ctg.fa
 
 mkdir -p "$OUTDIR"
  cd "$OUTDIR" 
